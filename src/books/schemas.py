d8f34ca7206b5bbdb135fa02e1aba295b6ad8185
Minhaza <minhazaaa1006@gmail.com>
2024-11-05 14:55:02 +0700

    Add model_config for BookCreateModel

--- src/books/schemas.py
+++ src/books/schemas.py
@@ -21,6 +21,19 @@ class BookCreateModel(BaseModel):
     page_count: int
     language: str
     
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "test",
+                "author": "author_test",
+                "publisher": "publisher_test",
+                "published_date": "2024-05-11",
+                "page_count": 100,
+                "language": "English"
+            }
+        }
+    }
+    
 class BookUpdateModel(BaseModel):
     title: str
     author: str
